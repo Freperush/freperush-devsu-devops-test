ed739adc9a243f633ec9a5d89909244b43af60bf
Frederik Perez <freperush@MBP.local>
2025-07-01 23:43:36 -0500

    Estilo: formateo automático con autopep8

--- api/views.py
+++ api/views.py
@@ -3,12 +3,13 @@ from rest_framework.response import Response
 from .serializers import UserSerializer
 from .models import User
 
+
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
     def list(self, request):
-        serializer = self.get_serializer(self.get_queryset(), many = True)
+        serializer = self.get_serializer(self.get_queryset(), many=True)
         return Response(serializer.data)
 
     def retrieve(self, request, pk):
